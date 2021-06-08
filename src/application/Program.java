d5912ffb024cdd5a15340b0d4571b1122a65bec4
Paulo Almeida <estpaulovictora@live.com>
2021-06-08 14:43:18 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -30,11 +30,16 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("=== TEST 3: seller insert ===");
+		System.out.println("=== TEST 4: seller insert ===");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 		
+		System.out.println("=== TEST 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 
 }
