738845ccf354c885644ce600577dcc25de30125b
Paulo Almeida <estpaulovictora@live.com>
2021-06-08 14:50:27 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,8 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("=== TEST 1: seller findById ===");
@@ -40,6 +43,12 @@ public class Program {
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Update completed");
+		
+		System.out.println("=== TEST 5: seller update ===");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed");
 	}
 
 }
--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -7,7 +7,7 @@ import model.entities.Department;
 public interface DepartmentDao {
 	void insert(Department obj);
 	void update(Department obj);
-	void delateById(Integer id);
+	void deleteById(Integer id);
 	Department findById(Integer id);
 	List<Department> findAll();
 }
